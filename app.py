e1025caa9663ab3d674fde78588c219eda67632a
Kayla Swift <kaylaswift@Kaylas-MacBook-Air-3.local>
2024-02-05 23:20:22 -0500

    Added an image and styled the webpage.

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
-'''
-# Routing for your application.
-# Put your routes below this comment
-'''
+
 @app.route('/')
 def home():
     return 'My home page'
